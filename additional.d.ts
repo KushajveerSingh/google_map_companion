a1f288f5e6f8a29254ecc651e669bd70f054c8d2
KushajveerSingh <kushajreal@gmail.com>
2022-11-16 02:18:16 -0500

    render map

---
+++ additional.d.ts
@@ -0,0 +1,6 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    NEXT_PUBLIC_BASE_URL: string;
+    NEXT_PUBLIC_GOOGLE_MAP_API_KEY: string;
+  }
+}
--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,5 +1,25 @@
+import GoogleMapReact from 'google-map-react';
+import { Box, Paper, Typography, Rating, useMediaQuery } from '@mui/material';
+import LocationOnOutlineIcon from '@mui/icons-material/LocationOnOutlined';
+
 const Map = () => {
-  return <>Map</>;
+  const isMobile = useMediaQuery('(min-width: 600px');
+  const coordinates = { lat: 0, lng: 0 };
+
+  return (
+    <Box sx={{ height: '85vh', width: '100%' }}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        // options={() => {}}
+        // onChange={() => {}}
+        // onChildClick={() => {}}
+      ></GoogleMapReact>
+    </Box>
+  );
 };
 
 export default Map;
